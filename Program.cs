9fb4584e587349aa43459883d7fb56540be2c487
Nelson Silva <nelson.silva@farfetch.com>
2017-04-10 14:53:45 +0100

    check D

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CheckDigit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(calc_check_digit("13361938"));
+        }
+
+        static int calc_check_digit(string BI)
+        {
+            int i=2;
+            int sum=0;
+
+            foreach(char c in BI)
+            {
+                sum += (c - '0') * i;
+                i++;
+            }
+
+        return sum%11;
+        }
+    }
+}
